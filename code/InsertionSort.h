52ac4d62143bf16f26852be5cb1bf252209acb3e
Adela Selimović <adelaadria1@outlook.com>
2018-11-28 15:24:59 +0100

    Update InsertionSort.h

--- code/InsertionSort.h
+++ code/InsertionSort.h
@@ -1,6 +1,6 @@
 template <class T>
-void InsertionSort(T niz[], int brojac) {
-	for (int i = 1; i < brojac; i++) {
+void InsertionSort(T niz[], int velicina) {
+	for (int i = 1; i < velicina; i++) {
 		int j = i;
 		while (j > 0 && niz[j - 1] > niz[j]) {
 			T pom = niz[j - 1];
