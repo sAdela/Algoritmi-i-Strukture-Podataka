3ee1d8de5635fd3d762022874f6215a819e79dfe
Adela Selimović <adelaadria1@outlook.com>
2019-04-29 23:40:53 +0200

    Create PrioritetniRed.h

---
+++ Prioritetni Red (Priority Queue)/PrioritetniRed.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Cvor.h"
+
+template <class Tip>
+class PrioritetniRed{
+
+public:
+
+	virtual void Dodaj(Tip x) = 0;
+
+	virtual Tip ukloni() = 0;
+
+	virtual bool JeLiPrazan() = 0;
+
+	virtual void Print()=0;
+};
