e2456d6906563fb388e1178295986135bbc6c766
Adela Selimović <adelaadria1@outlook.com>
2019-04-29 23:44:50 +0200

    Create Lista.h

---
+++ Lista (List)/Lista.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+template <class Tip>
+class Lista 
+{
+public:
+	virtual void Dodaj(Tip x) = 0;
+
+	virtual Tip ukloni() = 0;
+
+	virtual bool isPrazan() = 0;
+
+	virtual Tip& operator[](int i) = 0;
+
+	virtual void Print() = 0;
+
+	virtual int count() = 0;
+
+	virtual bool sadrzi(Tip v) = 0;
+
+};
