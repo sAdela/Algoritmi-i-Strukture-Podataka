34be39c70f0ece8743489b48a5e19eb46e3e6d3e
Adela Selimović <adelaadria1@outlook.com>
2019-04-29 23:54:07 +0200

    Create Cvor.h

---
+++ Cvor (Node)/Cvor.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <class Tip>
+struct Cvor
+{
+	Cvor* next;
+	Tip info;
+	Cvor(Tip info, Cvor<Tip>* next) : info(info)
+	{
+		this->next = next;
+	}
+};
