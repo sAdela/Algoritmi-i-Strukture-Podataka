c576facfa704da213dfe95796b5d559a6b4ba53f
Adela Selimović <adelaadria1@outlook.com>
2019-04-30 15:14:09 +0200

    Create BSCvor.h

---
+++ BSCvor (BSNode)/BSCvor.h
@@ -0,0 +1,14 @@
+#pragma once
+
+template <class Kljuc>
+struct BSCvor{
+	Kljuc value;
+	BSCvor* lijevoDijete;
+	BSCvor* desnoDijete;
+
+	BSCvor(Kljuc value){
+		this->value = value;
+		this->lijevoDijete = nullptr;
+		this->desnoDijete = nullptr;
+	}
+};
